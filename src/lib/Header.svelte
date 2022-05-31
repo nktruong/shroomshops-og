9b3a017bd958d6a41cc204fdef8bd0277f35d098
nktruong <nathankhaitruong@gmail.com>
2022-05-31 19:56:22 -0400

    Added header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<header>
+  <section class="flex justify-end p-4 w-full">
+    <button type="button" class="bg-[#EE4266] text-gray-50 px-3 py-2 font-semibold text-xl">
+      login
+    </button>
+  </section>
+</header>
