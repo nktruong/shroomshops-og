73c868202d5ea6a25d6a8aec61e8d575635c8b3e
nktruong <nathankhaitruong@gmail.com>
2022-05-31 20:17:32 -0400

    added more info to shop card

--- src/lib/Shop.svelte
+++ src/lib/Shop.svelte
@@ -1,3 +1,6 @@
-<section
-  class="flex h-56 w-96 items-end rounded-xl bg-cover p-4 text-gray-50"
-/>
+<section class="flex h-56 w-96 items-end rounded-xl bg-cover p-4 text-gray-50">
+  <section class="flex flex-col">
+    <span class="text-lg">player name</span>
+    <span class="text-3xl font-semibold text-green-400">shop name</span>
+  </section>
+</section>
