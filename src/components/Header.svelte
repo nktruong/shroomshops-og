62c6a0d063128e2241fd4df383c35ea0f347dd9f
nktruong <nathankhaitruong@gmail.com>
2022-06-02 18:28:25 -0400

    added name, description and search bar to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,29 @@
 <header>
   <section class="flex justify-end p-4 w-full">
-    <button type="button" class="bg-[#EE4266] text-gray-50 px-3 py-2 font-semibold text-xl">
+    <button
+      type="button"
+      class="bg-[#EE4266] text-gray-50 px-3 py-2 font-semibold text-xl"
+    >
       login
     </button>
+  </section>
+  <section class="text-center pt-10 max-w-md m-auto">
+    <h1 class="text-5xl font-gloria">Shroomshops</h1>
+    <p class="text-xl font-inter pt-6">
+      Easily supply yourself with all the Shroomhearth goods you need for your
+      journey!
+    </p>
+    <section class="pt-10 flex gap-4 justify-center">
+      <label for="search" class="sr-only" />
+      <input
+        name="search"
+        type="search"
+        placeholder="Search blocks"
+        class="rounded-full border-black border-2 text-center text-xl w-48"
+      />
+      <button type="submit" aria-label="Search">
+        <i class="ph-magnifying-glass text-3xl" />
+      </button>
+    </section>
   </section>
 </header>
