42fa39b66aec3e8f32b9a8dae9f9ff2a332ea7d9
nktruong <nathankhaitruong@gmail.com>
2022-06-03 15:49:28 -0400

    Clean up

--- src/components/Shop.svelte
+++ src/components/Shop.svelte
@@ -5,11 +5,9 @@
 <section class="flex h-56 w-96 items-end rounded-xl bg-cover p-4 text-gray-800">
   <section class="flex flex-col">
     <span class="text-lg">{shop.owner}</span>
-    <a href="/shop/{shop.name}">
+    <a href="/shop/{shop.id}">
       <span class="text-3xl font-semibold text-orange-900">{shop.name}</span>
     </a>
     <span class="text-2xl">{shop.description}</span>
-    <span class="text-xl">{shop.productName}</span>
-    <span class="text-xl">{shop.productPrice}</span>
   </section>
 </section>
