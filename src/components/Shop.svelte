d02793945b0905782158039a8eeeb67245d5c0a7
nktruong <nathankhaitruong@gmail.com>
2022-06-04 19:27:41 -0400

    Fixed issue of shops' background images not loading automatically

--- src/components/Shop.svelte
+++ src/components/Shop.svelte
@@ -4,7 +4,8 @@
 
 <a href="/shop/{shop.slug}">
   <section
-    class="bg-[url('{shop.image}')] flex h-56 w-96 scale-125 items-end rounded-xl bg-cover p-4 text-gray-800"
+    style="background-image: url('{shop.image}')"
+    class=" flex h-56 w-96 scale-125 items-end rounded-xl bg-cover p-4 text-gray-800"
   >
     <section class="flex flex-col">
       <span class="text-lg text-gray-100">{shop.owner}</span>
