e6ad9d0066640bce24788379e31b80b15cb2866e
nktruong <nathankhaitruong@gmail.com>
2022-06-03 20:04:07 -0400

    Wrapping entire shop container in a link

--- src/components/Shop.svelte
+++ src/components/Shop.svelte
@@ -2,12 +2,14 @@
   export let shop
 </script>
 
-<section class="flex h-56 w-96 items-end rounded-xl bg-cover p-4 text-gray-800">
-  <section class="flex flex-col">
-    <span class="text-lg">{shop.owner}</span>
-    <a href="/shop/{shop.slug}">
-      <span class="text-3xl font-semibold text-orange-900">{shop.name}</span>
-    </a>
-    <span class="text-2xl">{shop.description}</span>
+<a href="/shop/{shop.slug}">
+  <section
+    class="bg-[url('https://res.cloudinary.com/nktruong/image/upload/v1654299514/buru_mddjtz.png')] flex h-56 w-96 items-end rounded-xl bg-cover p-4 text-gray-800"
+  >
+    <section class="flex flex-col">
+      <span class="text-lg text-gray-100">{shop.owner}</span>
+      <span class="text-3xl font-semibold text-yellow-300">{shop.name}</span>
+      <span class="text-2xl">{shop.description}</span>
+    </section>
   </section>
-</section>
+</a>
