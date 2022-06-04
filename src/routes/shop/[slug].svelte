8c61eb3380e6cce45714e9edc838050532c6c145
nktruong <nathankhaitruong@gmail.com>
2022-06-03 23:49:37 -0400

    Added new content to shop page with the new schema

--- src/routes/shop/[slug].svelte
+++ src/routes/shop/[slug].svelte
@@ -10,12 +10,27 @@
 <section
   class="mx-auto max-w-xl my-16 bg-gray-50 text-slate-800 rounded-lg shadow-sm py-4 px-6 font-inter"
 >
-  <h1 class="text-2xl font-bold text-center mb-8">{shop.name}</h1>
-  <section class="">
+  <p class="text-center text-lg pb-2">
+    {shop.coordinates}
+  </p>
+  <h1
+    class="text-3xl font-bold text-center mb-4 pb-4 text-yellow-700  border-b-2 border-slate-200"
+  >
+    {shop.name}
+  </h1>
+  <section>
+    <p class="text-center text-lg pb-8">{shop.conversionRates}</p>
     {#each products as product (product.id)}
       <section class="grid grid-cols-2 text-xl">
-        <span>{product.name}</span>
-        <span>{product.price}</span>
+        <section>
+          <img
+            src={product.image}
+            alt={product.name}
+            class="h-16 w-16 inline"
+          />
+          <span class="my-auto">{product.name}</span>
+        </section>
+        <span class="my-auto">{product.price}</span>
       </section>
     {/each}
   </section>
