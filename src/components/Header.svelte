8db35d44b9ea121f44c465b85f4ce00949cd0a34
nktruong <nathankhaitruong@gmail.com>
2022-06-03 23:47:41 -0400

    Made header elements larger

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,16 +2,16 @@
   <section class="flex justify-end p-4 w-full">
     <button
       type="button"
-      class="bg-[#EE4266] text-gray-50 px-3 py-2 font-semibold text-xl"
+      class="bg-[#EE4266] text-gray-50 px-3 py-2 font-semibold text-2xl"
     >
       login
     </button>
   </section>
-  <section class="text-center pt-10 max-w-md m-auto">
-    <h1 class="text-5xl font-gloria">
+  <section class="pt-10 max-w-md m-auto">
+    <h1 class="text-7xl font-gloria text-center">
       <a href="../">Shroomshops</a>
     </h1>
-    <p class="text-xl font-inter pt-6">
+    <p class="text-2xl font-inter pt-6 text-center">
       Easily supply yourself with all the Shroomhearth goods you need for your
       journey!
     </p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,7 +6,7 @@
 
 <Header />
 
-<section class="font-inter mx-auto mt-24 grid max-w-4xl grid-cols-2 gap-y-16">
+<section class="font-inter mx-auto mt-24 grid max-w-5xl grid-cols-2 gap-x-32">
   {#each shops as shop (shop.id)}
     <Shop {shop} />
   {/each}
