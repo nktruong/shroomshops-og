3f42e009fa8532d2f8c05a594d766205d402f214
nktruong <nathankhaitruong@gmail.com>
2022-06-05 17:27:29 -0400

    More style changes

--- src/routes/shop/[slug].svelte
+++ src/routes/shop/[slug].svelte
@@ -8,30 +8,44 @@
 <Header />
 
 <section
-  class="mx-auto max-w-xl my-16 bg-gray-50 text-slate-800 rounded-lg shadow-sm py-4 px-6 font-inter"
+  class="mx-auto max-w-3xl my-16 bg-gray-50 text-slate-800 rounded-lg shadow-sm py-8 px-6 font-inter"
 >
-  <p class="text-center text-lg pb-2">
-    {shop.coordinates}
-  </p>
   <h1
-    class="text-3xl font-bold text-center mb-4 pb-4 text-yellow-700  border-b-2 border-slate-200"
+    class="text-5xl font-bold text-center mb-4 py-6 text-amber-500 font-gloria"
   >
-    {shop.name}
+    Welcome to {shop.name}!
   </h1>
-  <section>
-    <p class="text-center text-lg pb-8">{shop.conversionRates}</p>
-    {#each products as product (product.id)}
-      <section class="grid grid-cols-2 text-xl">
+  <section class="text-lg">
+    <section
+      class="flex flex-col gap-y-4 px-4 py-8 bg-amber-900 text-amber-100 rounded-md shadow-inner"
+    >
+      <section>
+        <span>Owned by </span>
+        <span class="font-semibold">{shop.owner}</span>
+      </section>
+      <section>
+        <span>Located at: </span>
+        <span class="font-semibold">{shop.coordinates}</span>
+      </section>
+      <section>
+        <span>Conversions: </span>
+        <span class="font-semibold">{shop.conversionRates}</span>
+      </section>
+    </section>
+    <section class="grid grid-cols-2 text-xl my-8 gap-y-2">
+      {#each products as product (product.id)}
         <section>
-          <img
-            src={product.image}
-            alt={product.name}
-            class="h-16 w-16 inline"
-          />
-          <span class="my-auto">{product.name}</span>
+          {#if product.image}
+            <img
+              src={product.image}
+              alt={product.name}
+              class="h-16 w-16 inline p-2"
+            />
+          {/if}
+          <span class="my-auto font-medium pl-2">{product.name}</span>
         </section>
         <span class="my-auto">{product.price}</span>
-      </section>
-    {/each}
+      {/each}
+    </section>
   </section>
 </section>
