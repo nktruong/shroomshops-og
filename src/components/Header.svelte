6597fcd70cc7875e70dd8e9a467d08a16151169f
nktruong <nathankhaitruong@gmail.com>
2022-06-05 17:25:59 -0400

    Disabled login button and removed search bar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,17 @@
+<script>
+  let searchTerm
+</script>
+
 <header>
   <section class="flex justify-end p-4 w-full">
-    <button
+    <!-- <button
       type="button"
       class="bg-[#EE4266] text-gray-50 px-3 py-2 font-semibold text-2xl"
     >
       login
-    </button>
+    </button> -->
   </section>
-  <section class="pt-10 max-w-md m-auto">
+  <section class="pt-10 max-w-xl mx-auto">
     <h1 class="text-7xl font-gloria text-center">
       <a href="../">Shroomshops</a>
     </h1>
@@ -15,17 +19,5 @@
       Easily supply yourself with all the Shroomhearth goods you need for your
       journey!
     </p>
-    <section class="pt-10 flex gap-4 justify-center">
-      <label for="search" class="sr-only" />
-      <input
-        name="search"
-        type="search"
-        placeholder="Search blocks"
-        class="rounded-full border-black border-2 text-center text-xl w-48"
-      />
-      <button type="submit" aria-label="Search">
-        <i class="ph-magnifying-glass text-3xl" />
-      </button>
-    </section>
   </section>
 </header>
