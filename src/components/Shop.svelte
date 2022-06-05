609b25c44a647e3753c4601323229f1f6bf14548
nktruong <nathankhaitruong@gmail.com>
2022-06-05 17:26:17 -0400

    Style changes

--- src/components/Shop.svelte
+++ src/components/Shop.svelte
@@ -5,11 +5,11 @@
 <a href="/shop/{shop.slug}">
   <section
     style="background-image: url('{shop.image}')"
-    class=" flex h-56 w-96 scale-125 items-end rounded-xl bg-cover p-4 text-gray-800"
+    class="flex h-56 w-96 items-end rounded-xl bg-cover p-4 text-gray-800"
   >
     <section class="flex flex-col">
       <span class="text-lg text-gray-100">{shop.owner}</span>
-      <span class="text-3xl font-semibold text-yellow-300">{shop.name}</span>
+      <span class="text-3xl font-semibold text-lime-100">{shop.name}</span>
       <span class="text-2xl">{shop.description}</span>
     </section>
   </section>
